42aa0163d62b5630f63fca95510d3612b6a66b46
Hector-rob <A01634105@itesm.mx>
2023-04-12 14:53:43 -0600

    Connected back to front / front side

--- ibmfront/src/Components/MainPage.js
+++ ibmfront/src/Components/MainPage.js
@@ -1,5 +1,36 @@
-export default function MainPage() {
-    return <div> Main Page
-       
-    </div>
-}
+
+import React, { Component } from 'react';
+
+class MainPage extends Component {
+    state = {
+        data: null
+      };
+    
+      componentDidMount() {
+        this.callBackendAPI()
+          .then(res => this.setState({ data: res.express }))
+          .catch(err => console.log(err));
+      }
+        // fetching the GET route from the Express server which matches the GET route from server.js
+      callBackendAPI = async () => {
+        const response = await fetch('/express_backend');
+        const body = await response.json();
+    
+        if (response.status !== 200) {
+          throw Error(body.message) 
+        }
+        return body;
+      };
+    
+      render() {
+        return (
+        
+            <div>
+              <h1>Welcome to React</h1>
+            <p>{this.state.data}</p>
+          </div>
+        );
+      }
+    }
+    
+    export default MainPage;
