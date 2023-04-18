315a1ac8b186ab61432d27575e559880f2198866
Diego Sú Gómez <a01620476@itesm.mx>
2023-04-18 10:22:47 -0600

    Added country dropdown in register screen

--- ibmfront/src/Components/Register.js
+++ ibmfront/src/Components/Register.js
@@ -1,4 +1,4 @@
-import { Grid, TextField, Typography } from "@mui/material";
+import { FormControl, Grid, InputLabel, Select, TextField, Typography, MenuItem, } from "@mui/material";
 import Container from "@mui/material/Container";
 import Box from "@mui/material/Box";
 import { IconButton, InputAdornment } from "@mui/material";
@@ -10,9 +10,26 @@ import Button from "@mui/material/Button";
 import { Link } from "react-router-dom";
 
 export default function Register() {
-
+    const countries = ['Afghanistan', 'Albania', 'Algeria', 'Andorra', 'Angola', 'Antigua and Barbuda', 'Argentina', 'Armenia', 'Australia', 'Austria',
+        'Azerbaijan', 'The Bahamas', 'Bahrain', 'Bangladesh', 'Barbados', 'Belarus', 'Belgium', 'Belize', 'Benin', 'Bhutan', 'Bolivia', 'Bosnia and Herzegovina',
+        'Botswana', 'Brazil', 'Brunei', 'Bulgaria', 'Burkina Faso', 'Burundi', 'Cabo Verde', 'Cambodia', 'Cameroon', 'Canada', 'Central African Republic', 'Chad',
+        'Chile', 'China', 'Colombia', 'Comoros', 'Congo, Democratic Republic of the', 'Congo, Republic of the', 'Costa Rica', 'Côte d Ivoire', 'Croatia', 'Cuba',
+        'Cyprus', 'Czech Republic', 'Denmark', 'Djibouti', 'Dominica', 'Dominican Republic', 'East Timor (Timor-Leste)', 'Ecuador', 'Egypt', 'El Salvador',
+        'Equatorial Guinea', 'Eritrea', 'Estonia', 'Eswatini', 'Ethiopia', 'Fiji', 'Finland', 'France', 'Gabon', 'The Gambia', 'Georgia', 'Germany', 'Ghana',
+        'Greece', 'Grenada', 'Guatemala', 'Guinea', 'Guinea-Bissau', 'Guyana', 'Haiti', 'Honduras', 'Hungary', 'Iceland', 'India', 'Indonesia', 'Iran', 'Iraq', 'Ireland',
+        'Israel', 'Italy', 'Jamaica', 'Japan', 'Jordan', 'Kazakhstan', 'Kenya', 'Kiribati', 'Korea, North', 'Korea, South', 'Kosovo', 'Kuwait', 'Kyrgyzstan', 'Laos',
+        'Latvia', 'Lebanon', 'Lesotho', 'Liberia', 'Libya', 'Liechtenstein', 'Lithuania', 'Luxembourg', 'Madagascar', 'Malawi', 'Malaysia', 'Maldives', 'Mali', 'Malta',
+        'Marshall Islands', 'Mauritania', 'Mauritius', 'Mexico', 'Micronesia, Federated States of', 'Moldova', 'Monaco', 'Mongolia', 'Montenegro', 'Morocco', 'Mozambique',
+        'Myanmar (Burma)', 'Namibia', 'Nauru', 'Nepal', 'Netherlands', 'New Zealand', 'Nicaragua', 'Niger', 'Nigeria', 'North Macedonia', 'Norway', 'Oman', 'Pakistan',
+        'Palau', 'Panama', 'Papua New Guinea', 'Paraguay', 'Peru', 'Philippines', 'Poland', 'Portugal', 'Qatar', 'Romania', 'Russia', 'Rwanda', 'Saint Kitts and Nevis',
+        'Saint Lucia', 'Saint Vincent and the Grenadines', 'Samoa', 'San Marino', 'Sao Tome and Principe', 'Saudi Arabia', 'Senegal', 'Serbia', 'Seychelles', 'Sierra Leone',
+        'Singapore', 'Slovakia', 'Slovenia', 'Solomon Islands', 'Somalia', 'South Africa', 'Spain', 'Sri Lanka', 'Sudan', 'Sudan, South', 'Suriname', 'Sweden', 'Switzerland',
+        'Syria', 'Taiwan', 'Tajikistan', 'Tanzania', 'Thailand', 'Togo', 'Tonga', 'Trinidad and Tobago', 'Tunisia', 'Turkey', 'Turkmenistan', 'Tuvalu', 'Uganda',
+        'Ukraine', 'United Arab Emirates', 'United Kingdom', 'United States', 'Uruguay', 'Uzbekistan', 'Vanuatu', 'Vatican City', 'Venezuela', 'Vietnam', 'Yemen', 'Zambia', 'Zimbabwe']
     const [visiblePassword, toggleVisibility] = useState(false);
     const clickHandler = () => toggleVisibility(!visiblePassword);
+    const [country, selectCountry] = useState("");
+    const selectCountryHandler = (val) => selectCountry(val.target.value);
 
     return <Container maxWidth disableGutters="true">
         <Grid container>
@@ -21,8 +38,8 @@ export default function Register() {
                     Back
                 </Button>
                 <Typography fontSize={50} sx={{ mt: 3, ml: 2 }}>Create a new <Box fontWeight={800} display='inline'>account</Box></Typography>
-                <Typography fontSize={30} sx={{ mt: 4, ml: 3, mb: 6}}>Exclusive to <Box fontWeight={800} display='inline'>IBM Managers</Box></Typography>
-                <img src="./Assets/images/pixelBG.webp" alt="Art" sx={{bottom: 0}}></img>
+                <Typography fontSize={30} sx={{ mt: 4, ml: 3, mb: 6 }}>Exclusive to <Box fontWeight={800} display='inline'>IBM Managers</Box></Typography>
+                <img src="./Assets/images/pixelBG.webp" alt="Art" sx={{ bottom: 0 }}></img>
             </Grid>
             <Grid item xs={6} sx={{ bgcolor: "white", mt: 5 }} textAlign={"center"}>
                 <Typography fontSize={30} sx={{ mt: 5 }}>Create a new <Box fontWeight={800} display='inline'>IBM Dashboard Manager</Box> account</Typography>
@@ -87,14 +104,27 @@ export default function Register() {
                                 }} />
                         </Grid>
                         <Grid item xs={6} sx={{ mt: 3 }}>
-                            <TextField sx={{ width: 275 }}
-                                variant="filled"
-                                required
-                                id="country"
-                                label="Country"
-                                name="country"
-                                autoComplete="country"
-                                autoFocus />
+                            <FormControl required>
+                                <InputLabel id="countryLabel">Country</InputLabel>
+                                <Select
+                                    sx={{ width: 275 }}
+                                    variant="filled"
+                                    
+                                    labelId="countryLabel"
+                                    id="selectCountry"
+                                    value={country}
+                                    label="Country"
+                                    onChange={selectCountryHandler}
+
+                                >
+                                    {countries.map((countryName) => (
+                                        <MenuItem
+                                            key={countryName}
+                                            value={countryName}
+                                        >{countryName}</MenuItem>
+                                    ))}
+                                </Select>
+                            </FormControl>
                         </Grid>
                         <Grid item xs={6} sx={{ mt: 3 }}>
                             <TextField
