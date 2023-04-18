1d0c52e7d67ae43bbbaea7137d73b1d153c07489
Hector-rob <A01634105@itesm.mx>
2023-04-18 17:04:36 -0600

    kk

--- ibmfront/.next/static/webpack/webpack.4eaa5d71dacffaec.hot-update.js
+++ ibmfront/.next/static/webpack/webpack.4eaa5d71dacffaec.hot-update.js
@@ -11,8 +11,21 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "d613c5c74b5147e9"; }
+/******/ 	__webpack_require__.h = function() { return "c7bd0977c615ef06"; }
 /******/ }();
+/******/ 
+/******/ /* webpack/runtime/compat */
+/******/ 
+/******/ 
+/******/ // noop fns to prevent runtime errors during initialization
+/******/ if (typeof self !== "undefined") {
+/******/ 	self.$RefreshReg$ = function () {};
+/******/ 	self.$RefreshSig$ = function () {
+/******/ 		return function (type) {
+/******/ 			return type;
+/******/ 		};
+/******/ 	};
+/******/ }
 /******/ 
 /******/ }
 );
