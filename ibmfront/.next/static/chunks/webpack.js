561ce7f98affbcdf7df8664c0e527fec4c63fe89
Hector-rob <A01634105@itesm.mx>
2023-05-16 13:03:48 -0600

    merged table view and export

--- ibmfront/.next/static/chunks/webpack.js
+++ ibmfront/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "75ac314b96e27925"; }
+/******/ 		__webpack_require__.h = function() { return "9cc4808874a502fe"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- ibmfront/.next/static/webpack/webpack.bf7d40ff42e5c541.hot-update.js
+++ ibmfront/.next/static/webpack/webpack.bf7d40ff42e5c541.hot-update.js
@@ -23,7 +23,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ 
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "75ac314b96e27925"; }
+/******/ 	__webpack_require__.h = function() { return "9cc4808874a502fe"; }
 /******/ }();
 /******/ 
 /******/ }
