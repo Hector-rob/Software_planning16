78f2fa4bcef699ef7f851910cf73dfebcb7673f2
Diego Sú Gómez <a01620476@itesm.mx>
2023-05-24 15:55:55 -0600

    Updated dependencies

--- ibmfront/.next/static/chunks/webpack.js
+++ ibmfront/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "4f2da2512e54f3eb"; }
+/******/ 		__webpack_require__.h = function() { return "62bae41a97d18ce5"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
